8c7e4c21f7747a2011a5453f4be9a1f250069bb3
nixon <nixondutta402@gmail.com>
2022-09-23 17:09:55 +0900

    _init__.py add

--- libcore/__init__.py
+++ libcore/__init__.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 from pathlib import Path
 import libcore.job
 import libcore.capture
@@ -10,8 +11,18 @@ from libcore import _version
 __version__ = _version.__version__
 
 def notify(notification, *args, **kwargs):
+    """
+    Make a notification to the user
+    
+    Args:
+        notification(list or dict): dicts must be encodable to JSON.
+    Example:
+        >>> import libcore
+        >>> libcore.notify([{'msg':'Hello'}])
+        [{"msg":"Hello!"}]
+    """
     kwargs['flush'] = True
-    # print(json.dumps(notification), *args, **kwargs)
+    print(json.dumps(notification), *args, **kwargs)
 
 _default_heartbeat_file = Path('/home/cybercore-pi/heartbeat')
 
@@ -21,7 +32,28 @@ def _default_heartbeat():
 _heartbeat_function = _default_heartbeat
 
 def set_heartbeat_function(f):
+    """
+    Set heartbeat action.
+
+    Args:
+        f(function): function which executes heartbeat action.
+
+    Example:
+        >>> import libcore
+        >>> def heartbeat(): print("working!")
+        ...
+        >>> libcore.set_heartbeat_function(heartbeat)
+        >>> libcore.heartbeat()
+        working!
+    """
+    global _heartbeat_function
     _heartbeat_function = f
 
 def heartbeat():
+    """
+    Execute heartbeat action.
+    
+    Notes:
+        Defalut action is 'touch /home/cybercore-pi/heartbeat'
+    """
     _heartbeat_function() 
